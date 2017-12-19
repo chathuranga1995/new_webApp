c5983c9c927f1aaff230e85dbf8eb160a0e2677c
Dinuka <dinukachathura@gmail.com>
2017-12-19 10:47:56 +0530

    first commit

---
+++ sign_in.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<div class="row">
+		<div class="col-md-12">
+			<h3>Sign In</h1>
+		</div>
+	</div>
+
+	<form>
+		<div class="row">
+			<div class="col-md-1">
+			</div>
+			<div class="col-md-3">
+				<h4>Name :</h4>
+			</div>
+			<div class="col-md-4">
+				<input type="text" placeholder="First"></input>
+			</div>
+			<div class="col-md-4">
+				<input type="text" placeholder="Last"></input>
+			</div>
+		</div>
+
+
+	</form>
+
+</body>
+</html>
---
+++ welcome.php
@@ -0,0 +1,69 @@
+<html>
+<head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+	<title>welcome</title>
+</head>
+<body>
+
+	<div class="container-fluid">
+		<div class="row">
+			<div>
+
+			</div>
+			<div class="col-md-4">
+
+			</div>
+			<div class="col-md-8">
+				<h1>WelCome!!!</h1>
+			</div>
+			
+		</div>
+		<br><br>
+		<form action="">
+		<div class="row">
+			<div class="col-md-2">
+				<h4>User Name :</h4>
+			</div>
+			<div class="col-md-10">
+				<input type="text" placeholder="username"></<input>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-2">
+				<h4>PassWord :</h4>
+			</div>
+			<div class="col-md-10">
+				<input type="text" placeholder="password"></input>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-2">
+			</div>
+			<div class="col-md-10">
+				<button class="btn btn-primary" >Log In</button>
+			</div>
+		</div>
+		</form>
+	 
+		<div class="row">
+			<div class="col-md-2">
+			</div>
+			<div class="col-md-10">
+				<form action="/sign_in.php">
+ 					<button class="btn btn-success">Sign In</button>
+				</form>
+				
+			</div>
+		</div>
+		
+		
+
+	</div>
+
+</body>
+</html>
